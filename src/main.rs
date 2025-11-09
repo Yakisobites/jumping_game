e15889f356fc9634b18b5127704e27889562dd76
IshiguroN <naoyai1204@icloud.com>
2025-11-09 18:48:20 +0900

    プロトタイプ作成

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,29 @@ const SCALE: f32 = 50.0; // [pixel/m]
 const CAMERA_HEIGHT:f32 = 100.0; // [pixel]
 const BALL_RADIUS: f32 = 0.3; // [m]
 const ARM_HALF_HEIGHT: f32 = 0.4; // [m]
-const ARM_RADIUS: f32 = 0.50; // [m]
-const FLOAR_THICKNESS: f32 = 0.2; // [m]
+const ARM_RADIUS: f32 = 0.25; // [m]
+const FLOAR_THICKNESS: f32 = 0.5; // [m]
+const FLOAR_WIDTH: f32 = 100.0; // [m]
 const TIME_DELTA: f32 = 1.0 / 1200.0; // [1/s]
 const KP: f32 = 50.0;
 const KD: f32 = 5.0;
 
+const PI: f32 = std::f32::consts::PI;
+const LOWER_WORLD_BOUND: f32 = -50.0; // [m]
+const ANGLE_THRESHOLD: f32 = 0.2; // [rad]
+
+
 // === 座標変換 ===
 // Rapierの世界座標（メートル単位）を画面座標（ピクセル）に変換
+/*
+    input: v: Vec4 (CenterX, CenterY, HalfWidth, HalfHeight)
+    output: Vec4 (TopLeftX, TopLeftY, Width, Height)
+*/
+fn world_to_screen_cubiod(v: Vec4, camera_pos: Vector<f32>) -> Vec4 {
+    // y軸を上下反転して、中心を画面中央に
+    vec4((v.x - v.z - camera_pos[0])*SCALE + screen_width() / 2.0, screen_height() - (v.y + v.w - camera_pos[1])*SCALE - CAMERA_HEIGHT, 2.0*v.z*SCALE, 2.0*v.w*SCALE)
+}
+
 fn world_to_screen(v: Vector<f32>, camera_pos: Vector<f32>) -> Vec2 {
     // y軸を上下反転して、中心を画面中央に
     vec2(v.x * SCALE + screen_width() / 2.0 - camera_pos[0] * SCALE, screen_height() - v.y * SCALE - CAMERA_HEIGHT + camera_pos[1] * SCALE)
@@ -38,8 +53,12 @@ async fn main() {
     let event_handler = ();
 
     // === 床を作成 ===
-    let ground = ColliderBuilder::cuboid(100.0, FLOAR_THICKNESS / 2.0).build();
+    let ground = ColliderBuilder::cuboid(FLOAR_WIDTH, FLOAR_THICKNESS / 2.0).build();
     collider_set.insert(ground);
+
+    // === オブジェクトを作成 ===
+    let object = ColliderBuilder::cuboid(1.0, 5.0 / 2.0).translation(vector![5.0, 5.0 / 2.0]).build();
+    collider_set.insert(object);
 
     // === Ferris君作成 ===
     // 胴体の作成
@@ -65,7 +84,7 @@ async fn main() {
     let texture = load_texture("assets/ferris.png").await.unwrap();
     texture.set_filter(FilterMode::Nearest);
 
-    let tex_scale = ((BALL_RADIUS + ARM_RADIUS + ARM_HALF_HEIGHT / 2.0) * 2.0 * SCALE) / texture.width() as f32;
+    let tex_scale = ((BALL_RADIUS + ARM_RADIUS + ARM_HALF_HEIGHT) * 2.0 * SCALE) / texture.width() as f32;
 
     let mut camera_pos = vector![0.0, 0.0];
     let mut camera_vel = vector![0.0, 0.0];
@@ -90,7 +109,6 @@ async fn main() {
         );
 
         // --- ボールを描画 ---
-        // let ball = &rigid_body_set[ball_handle];
         let ball = rigid_body_set.get_mut(ball_handle).unwrap();
         let pos = ball.translation();
         let angle = ball.rotation().angle(); // 角度
@@ -109,14 +127,16 @@ async fn main() {
         );
 
         // --- 床を描画 ---
-        let ground_y = world_to_screen(vector![0.0, - FLOAR_THICKNESS / 2.0], camera_pos).y;
-        draw_rectangle(0.0, ground_y, screen_width(), FLOAR_THICKNESS * SCALE, DARKGRAY);
+        let ground_plane_params = world_to_screen_cubiod(vec4(0.0, 0.0, FLOAR_WIDTH, FLOAR_THICKNESS / 2.0), camera_pos);
+        draw_rectangle(ground_plane_params.x, ground_plane_params.y, ground_plane_params.z, ground_plane_params.w, DARKGRAY);
+
+
+        // オブジェクトを描画
+        let object_params = world_to_screen_cubiod(vec4(5.0, 5.0 / 2.0, 1.0, 5.0 / 2.0), camera_pos);
+        draw_rectangle(object_params.x, object_params.y, object_params.z, object_params.w, BLUE);
         // --- 高さをコンソールに出す（デバッグ） ---
-        // println!("Ball altitude: {:.2}", pos.y);
-        // println!("Ball rotation: {:.2}", angle);
-        // println!("Ball placement: {:.2}", pos.x);
-        // println!("Camera altitude: {:.2}", camera_pos[1]);
-        // println!("Camera placement: {:.2}", camera_pos[0]);
+        println!("Ball altitude: {:.2}", pos.y);
+        println!("Ball placement: {:.2}", pos.x);
         // cameraをFerrisくんにフォーカスするようにする
         let target = vector![pos.x, pos.y];
         let error = target - camera_pos;
@@ -130,46 +150,45 @@ async fn main() {
             break;
         }
 
+        // 高さ下限
+        if pos.y < LOWER_WORLD_BOUND {
+            break;
+        }
+
+        // 角度制限
+        if PI - angle.abs() < ANGLE_THRESHOLD {
+            break;
+        }
+
+        // 速度上限
+        let vel = ball.linvel();
+        let speed = vel.norm();
+
+        if speed > 10.0 {
+            let keep_vel = vel / speed * 10.0;
+            ball.set_linvel(keep_vel, true);
+        }
+
         if is_key_down(KeyCode::Up) {
             ball.reset_forces(true); // Reset the forces to zero.
             ball.reset_torques(true); // Reset the torques to zero.
-            // ball.add_force(vector![0.0, 10.0], true);
-            // ball.add_torque(0.0, true);
-            // ball.add_force_at_point(vector![0.0, 10.0], point![1.0, 2.0], true);
 
             ball.apply_impulse(vector![0.0, 0.05], true);
-            // ball.apply_torque_impulse(100.0, true);
-            // ball.apply_impulse_at_point(vector![0.0, 10.0], point![1.0, 2.0], true);
         }
 
         if is_key_down(KeyCode::Left) {
             ball.reset_forces(true); // Reset the forces to zero.
             ball.reset_torques(true); // Reset the torques to zero.
-            // ball.add_force(vector![0.0, 10.0], true);
-            // ball.add_torque(0.0, true);
-            // ball.add_force_at_point(vector![0.0, 10.0], point![1.0, 2.0], true);
 
             ball.apply_impulse(vector![-0.03, 0.0], true);
-            // ball.apply_torque_impulse(100.0, true);
-            // ball.apply_impulse_at_point(vector![0.0, 10.0], point![1.0, 2.0], true);
         }
-        // ball.reset_forces(true); // Reset the forces to zero.
-        // ball.reset_torques(true); // Reset the torques to zero.
 
         if is_key_down(KeyCode::Right) {
             ball.reset_forces(true); // Reset the forces to zero.
             ball.reset_torques(true); // Reset the torques to zero.
-            // ball.add_force(vector![0.0, 10.0], true);
-            // ball.add_torque(0.0, true);
-            // ball.add_force_at_point(vector![0.0, 10.0], point![1.0, 2.0], true);
 
             ball.apply_impulse(vector![0.03, 0.0], true);
-            // ball.apply_torque_impulse(100.0, true);
-            // ball.apply_impulse_at_point(vector![0.0, 10.0], point![1.0, 2.0], true);
         }
-        // ball.reset_forces(true); // Reset the forces to zero.
-        // ball.reset_torques(true); // Reset the torques to zero.
-
         next_frame().await;
     }
 }
